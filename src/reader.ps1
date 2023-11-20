6b8ed8659b372a19221e59ecf58280bc28bad0dc
AntoFlor <florentantoine32@gmail.com>
2023-11-20 10:16:26 +0100

    base config

---
+++ src/reader.ps1
@@ -0,0 +1,8 @@
+function menu{
+    Write-Host ""
+    Write-Host "########################################"
+    Write-Host "###        Our custom script         ###"
+    Write-Host "########################################"
+    Write-Host ""
+    Write-Host "1) Set name "
+}
