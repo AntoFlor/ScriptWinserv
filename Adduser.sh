1b37f372b56d7fe3c14243fe64ca1559cf34a90b
Corsinix <99492939+Corsinix@users.noreply.github.com>
2023-11-20 11:07:54 +0100

    Create Adduser.sh

---
+++ Adduser.sh
@@ -0,0 +1,42 @@
+# Importer les données depuis le fichier CSV
+$users = Import-Csv -Path ".\users.csv"
+
+# Créer les unités d'organisation (OU) pour chaque département
+$departments = $users | Select-Object -Property Department -Unique
+
+foreach ($department in $departments) {
+    $ouName = $department.Department
+    New-ADOrganizationalUnit -Name $ouName -Path "DC=example,DC=com"
+}
+
+# Créer les groupes pour chaque département
+foreach ($department in $departments) {
+    $groupName = $department.Department + " Group"
+    New-ADGroup -Name $groupName -GroupScope Global -Path "OU=" + $department.Department + ",DC=example,DC=com"
+}
+
+# Créer les utilisateurs
+foreach ($user in $users) {
+    $userParams = @{
+        SamAccountName = $user.DisplayName
+        UserPrincipalName = $user.upn + "@example.com"
+        Name = $user.DisplayName
+        GivenName = $user.givenname
+        Surname = $user.surname
+        Department = $user.Department
+        Country = $user.country
+        Path = "OU=" + $user.Department + ",DC=example,DC=com"
+    }
+
+    New-ADUser @userParams
+
+    # Ajouter l'utilisateur au groupe de son département
+    $group = Get-ADGroup -Filter { Name -eq ($user.Department + " Group") }
+    Add-ADGroupMember -Identity $group -Members $user.DisplayName
+    
+    # Créer un dossier partagé pour l'utilisateur dans le format "D:\Shared\<Department>\<Username>"
+    $folderPath = "D:\Shared\" + $user.Department + "\" + $user.DisplayName
+    New-Item -ItemType Directory -Path $folderPath
+
+    }
+    
